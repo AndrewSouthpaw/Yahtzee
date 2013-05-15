caefd9180f1750bca204bdd490a372d0ccec92a9
S <s>
2013-05-15 13:04:11 -0700

    {"type":"TI", "runs":"1"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -83,10 +83,11 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	
 	private void rollDice(int roll, int[] dice) {
 		for (int i = 0; i < N_DICE; i++) {
-			if (roll != 0 && display.isDieSelected(i)) break;
-			IODialog dialog = getDialog();
-			int entry = dialog.readInt("Enter a value for dice #" + (i + 1));
-			dice[i] = entry;
+			if (roll == 0 || display.isDieSelected(i)) {
+				IODialog dialog = getDialog();
+				int entry = dialog.readInt("Enter a value for dice #" + (i + 1));
+				dice[i] = entry;				
+			}
 		}
 	}
 		
