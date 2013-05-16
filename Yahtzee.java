edfe71334ca5c66373c1147502339942a20e1661
S <s>
2013-05-15 17:36:47 -0700

    {"type":"TI", "runs":"12"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -109,16 +109,80 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
 	}
 	
 	private int calculateCategoryScore(int category, int[] dice) {
-		int score = dialog.readInt("Enter a score:");
-/*		int score = 0;
+		boolean b = YahtzeeMagicStub.checkCategory(dice, category);
+		int score = 0;
 		
 		if (b) {
+			if (category >= ONES && category <= SIXES) {
+				return sumDice(dice, category);
+			}
 			
-			return score;
+			switch (category) {
+				case THREE_OF_A_KIND: if(isNOfAKind(3, dice, false)) return sumDice(dice, 0);
+				case FOUR_OF_A_KIND: if(isNOfAKind(4, dice, false)) return sumDice(dice, 0);
+				case FULL_HOUSE: if(isNOfAKind(3, dice, true) && isNOfAKind(2, dice, true)) return FULL_HOUSE_SCORE;
+				case SMALL_STRAIGHT: if(isStraight(4, dice)) return SMALL_STRAIGHT_SCORE;
+				case LARGE_STRAIGHT: if(isStraight(5, dice)) return LARGE_STRAIGHT_SCORE;
+				case YAHTZEE: if(isNOfAKind(5, dice, false)) return YAHTZEE_SCORE;
+				case CHANCE: sumDice(dice, 0);
+			}
+			
+			// score = dialog.readInt("Valid category. Enter a score:");
+			
 		} else {
+			display.printMessage("Invalid category. Score is 0.");
 			
-		} */
+		} 
 		return score;
+	}
+	
+	
+	private boolean isNOfAKind(int n, int[] dice, boolean exact) {
+		boolean result = false;
+		int[] frequency = diceValueFrequency(dice);
+		for (int i = 0; i < frequency.length; i++) {
+			if (exact) {
+				if (frequency[i] == n) return true;
+			} else {
+				if (frequency[i] >= n) return true;
+			}
+		}
+		return result;
+	}
+	
+	private int[] diceValueFrequency(int[] dice) {
+		int[] result = new int[6];
+		for (int i = 0; i < N_DICE; i++) {
+			result[dice[i] - 1]++;
+		}
+		return result;
+	}
+	
+	private int sumDice(int[] dice, int dieValueRequirement) {
+		int result = 0;
+		for (int i = 0; i < N_DICE; i++) {
+			if (dieValueRequirement == 0) {
+				// Sum all dice
+				result += dice[i];
+			} else {
+				// Add die if it matches the required value
+				if (dice[i] == dieValueRequirement) result += dice[i];
+			}
+		}
+		return result;
+	}
+	
+	
+	private boolean isStraight(int n, int[] dice) {
+		int[] frequency = diceValueFrequency(dice);
+		for (int i = 0; i < (frequency.length - n + 1); i++) {
+			int nInARow = 0;
+			for (int j = 0; j < n; j++) {
+				if (frequency[i + j] > 0) nInARow++;
+			}
+			if (nInARow == n) return true;
+		}
+		return false;
 	}
 		
 /* Private instance variables */
