88b8b82b630c42b1cddbd763088feb09825ba397
S <s>
2013-05-18 14:02:18 -0700

    {"type":"TI", "runs":"0"}

--- CategoryResult.java
+++ CategoryResult.java
@@ -1,4 +1,32 @@
+/**
+ * CategoryResult keeps track of the category selection and whether the
+ * dice fulfill the requirements for that category. 
+ */
 
 public class CategoryResult {
-
+	
+	public CategoryResult() {
+		
+	}
+	
+	public void setCategory(int n) {
+		category = n;
+	}
+	
+	public void setValid(boolean b) {
+		validity = b;
+	}
+	
+	public int getCategory() {
+		return category;
+	}
+	
+	public boolean isValid() {
+		return validity;
+	}
+	
+/* Private instance variables */
+	private int category;
+	private boolean validity;
+	
 }
