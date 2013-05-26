33bd0896c533b7cba2da5f6e7d1367f115b7e0db
S <s>
2013-05-26 10:22:27 -0700

    {"type":"TI", "runs":"0"}

--- YahtzeeAI_v1.java
+++ YahtzeeAI_v1.java
@@ -1,4 +1,388 @@
+/**
+ * Provides the methods for a Yahtzee computer player.
+ * 
+ * Debugging: set as own program.
+ */
 
-public class YahtzeeAI_v1 {
+import acm.program.*;
+import acm.util.*;
+import java.util.*;
 
+public class YahtzeeAI_v1 extends ConsoleProgram implements YahtzeeConstants {
+
+	public void run() {
+		playGame();
+		
+	}
+	
+	private void playGame() {
+		boolean gameOver = false;
+		scorecard = new int[N_CATEGORIES + 1][nPlayers + 1];
+		categoryHasBeenChosen = new boolean[N_CATEGORIES + 1][nPlayers + 1];
+		int round = 1;
+		while (!gameOver) {
+			playRound(round);
+			if (round == N_SCORING_CATEGORIES) gameOver = true;
+			round++;
+		}
+		//int winner = highestScoringPlayer();
+		println("Congratulations, you are the winner with a total score of " + scorecard[TOTAL][1] + "!");
+		pause(DELAY);
+		
+		
+	}
+	
+/**
+ * Plays a round, which is one turn for every player.
+ * @param round The round number
+ */
+	private void playRound(int round) {
+		for (int i = 1; i <= nPlayers; i++) {
+			playTurn(i, round);
+			evaluateTotalScores(i, round);
+			printScorecard(i);
+			String str = readLine("Press enter to continue.");
+		}
+		//if (nPlayers > 1) announcePlayerInTheLead();
+	}
+	
+	
+/**
+ * Plays a turn for one player. Includes rolling dice, and picking category for scoring.
+ * @param player The player whose turn it is (index base 1)
+ * @param round The round number
+ */
+	public void playTurn(int player, int round) {
+		println("Playing round " + round);
+		int[] dice = new int[N_DICE];
+		boolean[] diceSelections = new boolean[N_DICE];
+		//display.waitForPlayerToClickRoll(player);
+		for (int rolls = 0; rolls < MAX_ROLLS; rolls++) {
+			println("Rolling dice...");
+			pause(delay);
+			rollDice(rolls, dice, diceSelections);
+			//display.displayDice(dice);
+			println("Dice for roll " + rolls + ": " + diceToString(dice));
+			if (rolls == MAX_ROLLS - 1) break;
+			diceSelections = selectDice(dice);
+			println("Selections for next roll: " + selectionsToString(diceSelections));
+		}
+		println("Turn is over.");
+		pause(delay);
+		int category = chooseBestCategory(player, dice);
+		categoryHasBeenChosen[category][player] = true;
+		println("Choosing category " + category);
+		boolean isValid = isDiceValidForCategory(dice, category);
+		println("Dice are valid for this category: " + isValid);
+		int score = calculateCategoryScore(category, isValid, dice);
+		println("Score for this category: " + score);
+		updateScore(player, category, score);
+		pause(DELAY);
+		
+		/*
+		CategoryResult result = chooseCategory(player, dice);
+		int category = result.getCategory();
+		boolean isValid = result.isValid();
+		int score = calculateCategoryScore(category, isValid, dice);
+		updateScore(player, category, score);
+		*/
+	}
+	
+/**
+ * Rolls the dice that are selected (or all the dice on the first roll).
+ * @param roll The number of rolls that have occurred
+ * @param dice The set of dice
+ */
+	private void rollDice(int roll, int[] dice, boolean[] isDieSelected) {
+		for (int i = 0; i < N_DICE; i++) {
+			if (roll == 0 || isDieSelected[i]) {
+				int die = rgen.nextInt(1, 6);
+				dice[i] = die;				
+			}
+		}
+	}
+	
+/**
+ * Returns a string version of the dice.
+ */
+	private String diceToString(int[] dice) {
+		String result = "[ ";
+		for (int i = 0; i < dice.length; i++) {
+			result += dice[i] + " ";
+		}
+		result += "]";
+		return result;
+	}
+	
+	private String selectionsToString(boolean[] selections) {
+		String result = "[ ";
+		for (int i = 0; i < selections.length; i++) {
+			result += selections[i] + " ";
+		}
+		result += "]";
+		return result;
+	}
+	
+/** Stub. Initially dice are selected at random. */
+	private boolean[] selectDice(int[] dice) {
+		boolean[] result = new boolean[N_DICE];
+		for (int i = 0; i < dice.length; i++) {
+			result[i] = rgen.nextBoolean();
+		}
+		return result;
+	}
+	
+/** Creates a list of all possible dice combinations */
+	/* Code here is not currently generalizeable for N_DICE. Need to fix. */
+	private void generateAllDiceCombinations() {
+		
+		for (int d1 = 1; d1 <= 6; d1++) {
+			for (int d2 = d1; d2 <= 6; d2++) {
+				for (int d3 = d2; d3 <= 6; d3++) {
+					for (int d4 = d3; d4 <= 6; d4++) {
+						for (int d5 = d4; d5 <= 6; d5++) {
+							int[] arr = new int[N_DICE];
+							arr[0] = d1;
+							arr[1] = d2;
+							arr[2] = d3;
+							arr[3] = d4;
+							arr[4] = d5;
+							DiceCombination combo = new DiceCombination(arr);
+						}
+					}
+				}
+			}
+		}
+		
+	}
+	
+	
+	
+	
+	
+/** Selects the highest scoring category */
+	private int chooseBestCategory(int player, int[] dice) {
+		int categoryIndex = 0;
+		int highestScore = -1;
+		for (int i = 1; i < 16; i++) {  // sloppy, fix later.
+			if (categoryHasBeenChosen[i][player] == false) {
+				boolean isValid = isDiceValidForCategory(dice, i);
+				int score = calculateCategoryScore(i, isValid, dice);
+				if (score > highestScore) {
+					highestScore = score;
+					categoryIndex = i;
+				}
+				if (i == 6) i = 8; // sloppy, fix later.
+			}
+		}
+		
+		return categoryIndex;
+	}
+	
+	
+/**
+ * Determines whether the dice fulfill the requirements of a category.
+ * @param dice The set of dice
+ * @param category The category in question
+ * @return Whether the dice fulfill the requirements of a category
+ */
+	private boolean isDiceValidForCategory(int[] dice, int category) {
+		if (category >= ONES && category <= SIXES) {
+			for (int i = 0; i < N_DICE; i++) {
+				if (dice[i] == category) return true;
+			}
+		}
+		switch (category) {
+			case THREE_OF_A_KIND: return isNOfAKind(3, dice, false);
+			case FOUR_OF_A_KIND: return isNOfAKind(4, dice, false);
+			case FULL_HOUSE: return (isNOfAKind(3, dice, true) && isNOfAKind(2, dice, true));
+			case SMALL_STRAIGHT: return isStraight(4, dice);
+			case LARGE_STRAIGHT: return isStraight(5, dice);
+			case YAHTZEE: return isNOfAKind(5, dice, false);
+			case CHANCE: return true;
+			default: return false;
+		}
+	}
+	
+	
+/**
+ * Calculates the score for a category.
+ * @param category The selected category
+ * @param dice The set of dice
+ * @return The score
+ */
+	private int calculateCategoryScore(int category, boolean isValid, int[] dice) {
+		if (isValid) {
+			switch (category) {
+				case ONES:
+				case TWOS:
+				case THREES:
+				case FOURS:
+				case FIVES:
+				case SIXES: return sumDice(dice, category);
+				case THREE_OF_A_KIND: return sumDice(dice, 0);
+				case FOUR_OF_A_KIND: return sumDice(dice, 0);
+				case FULL_HOUSE: return FULL_HOUSE_SCORE;
+				case SMALL_STRAIGHT: return SMALL_STRAIGHT_SCORE;
+				case LARGE_STRAIGHT: return LARGE_STRAIGHT_SCORE;
+				case YAHTZEE: return YAHTZEE_SCORE;
+				case CHANCE: return sumDice(dice, 0);
+				default: return 0;
+			}
+			
+		} else {
+			return 0;
+		}
+	}
+	
+/**
+ * Determines whether there are N of the same die value showing
+ * @param n The number of dice with the same value
+ * @param dice The set of dice
+ * @param exact Whether it must be exactly N or at least N
+ * @return Whether there are N of a kind showing
+ */
+	private boolean isNOfAKind(int n, int[] dice, boolean exact) {
+		boolean result = false;
+		int[] frequency = diceValueFrequency(dice);
+		for (int i = 0; i < frequency.length; i++) {
+			if (exact) {
+				if (frequency[i] == n) return true;
+			} else {
+				if (frequency[i] >= n) return true;
+			}
+		}
+		return result;
+	}
+	
+/**
+ * Creates an array listing the frequency of each possible die value for the set of dice
+ * @param dice The set of dice
+ * @return An ordered array listing the frequency of each die value
+ */
+	private int[] diceValueFrequency(int[] dice) {
+		int[] result = new int[6];
+		for (int i = 0; i < N_DICE; i++) {
+			result[dice[i] - 1]++;
+		}
+		return result;
+	}
+	
+/**
+ * Sums the dice.
+ * @param dice The set of dice
+ * @param dieValueRequirement Whether only a specific value should be summed. Enter '0' for no requirement (sum all)
+ * @return The sum of the dice
+ */
+	private int sumDice(int[] dice, int dieValueRequirement) {
+		int result = 0;
+		for (int i = 0; i < N_DICE; i++) {
+			if (dieValueRequirement == 0) {
+				// Sum all dice
+				result += dice[i];
+			} else {
+				// Add die if it matches the required value
+				if (dice[i] == dieValueRequirement) result += dice[i];
+			}
+		}
+		return result;
+	}
+	
+/**
+ * Determines whether the dice contain a straight (sequential) of a specific length.
+ * @param n The length of the straight
+ * @param dice The set of dice
+ * @return Whether the dice contain a straight
+ */
+	private boolean isStraight(int n, int[] dice) {
+		int[] frequency = diceValueFrequency(dice);
+		for (int i = 0; i < (frequency.length - n + 1); i++) {
+			int nInARow = 0;
+			for (int j = 0; j < n; j++) {
+				if (frequency[i + j] > 0) nInARow++;
+			}
+			if (nInARow == n) return true;
+		}
+		return false;
+	}
+	
+	
+/**
+ * Updates the players score on the scorecard and the display.
+ * @param player The player number (index base 1)
+ * @param category The category the score will be placed in
+ * @param score The score
+ */
+	private void updateScore(int player, int category, int score) {
+		scorecard[category][player] = score;
+	}
+	
+/**
+ * Evaluates total score fields.
+ * @param player The player number (index base 1)
+ * @param round The round number
+ */
+	private void evaluateTotalScores(int player, int round) {
+		updateScore(player, UPPER_SCORE, sumScores(player, ONES, SIXES));
+		updateScore(player, LOWER_SCORE, sumScores(player, THREE_OF_A_KIND, CHANCE));
+		updateScore(player, TOTAL, (scorecard[UPPER_SCORE][player] + scorecard[UPPER_BONUS][player] + 
+										scorecard[LOWER_SCORE][player]));
+		if (isUpperScoreComplete(player)) {
+			if (scorecard[UPPER_SCORE][player] >= 63) {
+				updateScore(player, UPPER_BONUS, UPPER_BONUS_SCORE);
+			} else {
+				updateScore(player, UPPER_BONUS, 0);
+			}
+		}
+	}
+	
+	
+/**
+ * Sums a set of scores on the scorecard.
+ * @param player The player number (index base 1)
+ * @param startCategory The starting category (inclusive)
+ * @param endCategory The ending category (inclusive)
+ * @return The sum of the scores
+ */
+	private int sumScores(int player, int startCategory, int endCategory) {
+		int result = 0;
+		for (int i = startCategory; i <= endCategory; i++) {
+			result += scorecard[i][player];
+			
+		}
+		return result;
+	}
+
+/**
+ * Determines if all categories in the upper score have been filled.
+ * @param player The player number (index base 1)
+ * @return Whether all the upper score categories have been filled
+ */
+	private boolean isUpperScoreComplete(int player) {
+		for (int i = ONES; i <= SIXES; i++) {
+			if (scorecard[i][player] == 0) return false;
+		}
+		return true;
+	}
+	
+/** Prints the scorecard */
+	private void printScorecard(int player) {
+		println("Printing scorecard...");
+		for (int i = 1; i <= N_CATEGORIES; i++) {
+			if (categoryHasBeenChosen[i][player] == true) {
+				println("[" + i + "]: " + scorecard[i][player]);
+			} else {
+				println("[" + i + "]: ");
+			}
+		}
+	}
+	
+	
+/* Private instance variables */
+	private int scorecard[][];
+	private final int nPlayers = 1;
+	private boolean[][] categoryHasBeenChosen;
+	private int delay = 500;
+	private final RandomGenerator rgen = RandomGenerator.getInstance();
+	private final ArrayList<DiceCombination> combosList = new ArrayList<DiceCombination>();
 }
