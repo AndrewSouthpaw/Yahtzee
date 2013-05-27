7dcb92e462e1708ddcb635f8bf07c431bfbb4b07
S <s>
2013-05-27 19:55:17 -0400

    {"type":"TI", "runs":"0"}

--- YahtzeeConstants.java
+++ YahtzeeConstants.java
@@ -23,7 +23,7 @@ public interface YahtzeeConstants {
 	public static final int MAX_PLAYERS = 4;
 	
 /** The number of times a player is allowed to roll the dice */
-	public static final int MAX_ROLLS = 3;
+	public static final int MAX_ROLLS = 2;
 
 /** The total number of categories */
 	public static final int N_CATEGORIES = 17;
