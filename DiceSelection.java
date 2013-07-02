81788730f80c146ad6eacca0aa8dee9f56997fe0
S <s>
2013-07-02 15:14:46 +0100

    {"type":"TI", "runs":"0"}

--- DiceSelection.java
+++ DiceSelection.java
@@ -40,6 +40,12 @@ public class DiceSelection implements YahtzeeConstants {
 		return name;
 	}
 	
+	
+	/** Returns the dice selection */
+	public boolean[] getSelection() {
+		return selection;
+	}
+	
 
 	
 	/* Private instance variables */
