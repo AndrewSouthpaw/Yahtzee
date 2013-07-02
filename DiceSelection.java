74dd5e32f66f4145a3fe6eb22b167f07520bf3cc
S <s>
2013-07-02 15:09:25 +0100

    {"type":"TI", "runs":"0"}

--- DiceSelection.java
+++ DiceSelection.java
@@ -1,4 +1,43 @@
 
-public class DiceSelection {
+/**
+ * The DiceSelection class stores a unique combination of selected dice for reroll.
+ * 
+ * Each combination contains a set of possible dice combinations that could result from rerolling
+ * with this dice selection. Each of these dice combinations has an associated evalue. These evalues
+ * are summed together to produce the evalue associated with the dice selection.
+ * 
+ */
 
+
+public class DiceSelection implements YahtzeeConstants {
+
+	/**
+	 * Constructor for DiceSelection.
+	 * @param arr The dice selections
+	 */
+	public DiceSelection(boolean[] arr) {
+		selection = arr;
+		name = createName();
+	}
+	
+	
+	
+	/**
+	 * Creates a unique name for the combination, which are the dice selections in String format
+	 * @return The name of the combination
+	 */
+	private String createName() {
+		String result = "";
+		for (int i = 0; i < selection.length; i++) {
+			result += selection[i];
+		}
+		return result;
+	}
+
+	
+	/* Private instance variables */
+	private boolean[] selection = new boolean[N_DICE];
+	private String name;
+	
+	
 }
